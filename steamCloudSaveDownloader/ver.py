4a510114feb9c5f255ac4ad7665010e1ed41b04a
Hojei Hsu <hhhhojeihsu@gmail.com>
2023-07-15 00:43:30 +0800

    Add Version Embedding

--- steamCloudSaveDownloader/ver.py
+++ steamCloudSaveDownloader/ver.py
@@ -1,4 +1,6 @@
 import importlib.metadata
+import sys
+import os
 
 __version__ = None
 
@@ -6,6 +8,18 @@ try:
     __version__ = importlib.metadata.version("scsd")
 except:
     pass
+
+def meipass_ver():
+    if not hasattr(sys, '_MEIPASS'):
+        return None
+    ver_file = os.path.join(sys._MEIPASS, 'version.txt')
+    if not os.path.isfile(ver_file):
+        return None
+    with open(ver_file) as f:
+        return f.readline().strip()
+
+if __version__ is None:
+    __version__ = meipass_ver()
 
 if __version__ is None:
     __version__ = "Version Unknown"
