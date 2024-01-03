1e4a71c3c72d1957a0bd484ed6947545f37a8dad
Hojei Hsu <hhhhojeihsu@gmail.com>
2024-01-03 22:49:23 +0800

    Support Email 2FA by code_type Override

--- steamCloudSaveDownloader/auth.py
+++ steamCloudSaveDownloader/auth.py
@@ -1,4 +1,5 @@
 #import steam.webauth as wa
+from http import HTTPStatus
 from steampy.login import LoginExecutor
 from steampy import guard
 import requests
@@ -12,7 +13,24 @@ import getpass
 def generate_one_time_code_override(p_input):
     return p_input
 
+def _update_steam_guard_override(self, login_response: requests.Response) -> None:
+    client_id = login_response.json()['response']['client_id']
+    steamid = login_response.json()['response']['steamid']
+    request_id = login_response.json()['response']['request_id']
+    code_type = 2
+    code = guard.generate_one_time_code(self.shared_secret)
+
+    update_data = {'client_id': client_id, 'steamid': steamid, 'code_type': code_type, 'code': code}
+    response = self._api_call(
+        'POST', 'IAuthenticationService', 'UpdateAuthSessionWithSteamGuardCode', params=update_data
+    )
+    if response.status_code == HTTPStatus.OK:
+        self._pool_sessions_steam(client_id, request_id)
+    else:
+        raise Exception('Cannot update Steam guard')
+
 guard.generate_one_time_code = generate_one_time_code_override
+LoginExecutor._update_steam_guard = _update_steam_guard_override
 
 class auth:
     s_session_filename = 'session.sb'
@@ -32,8 +50,7 @@ class auth:
             login_executor = LoginExecutor(
                 self.username,
                 self.password,
-                "",
-                self.session)
+                "")
             login_executor.login()
         except:
             pass
@@ -41,6 +58,7 @@ class auth:
         self.two_factor_code = input("2FA code (case insensitive): ")
         self.two_factor_code = self.two_factor_code.upper()
         try:
+            self.session = requests.Session()
             login_executor = LoginExecutor(
                 self.username,
                 self.password,
