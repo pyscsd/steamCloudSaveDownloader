9ec4f5d0cf5b5f730f9d5183932ba4ecc32999ee
Hojei Hsu <hhhhojeihsu@gmail.com>
2025-05-05 18:41:54 +0800

    Support Language in config

--- steamCloudSaveDownloader/config.py
+++ steamCloudSaveDownloader/config.py
@@ -36,7 +36,8 @@ Defaults = {
     'GUI': {
         "minimize_to_tray": (bool, True),
         "download_interval": (int, 30),
-        "auto_start": (bool, True)
+        "auto_start": (bool, True),
+        "language": (str, "en_US")
     }
 }
 
